00e2d23b19d51c6de5a5875815d52ce1d439d3f8
jardincyril <39823585+jardincyril@users.noreply.github.com>
2018-10-16 16:16:20 +0200

    Update streamChat.php

--- viewLoading/streamChat.php
+++ viewLoading/streamChat.php
@@ -4,7 +4,7 @@
         <div class="col s12 m8 l8 stream">
             <div class="video-container">
                 <!--ecran de stream-->
-                <iframe height="720" width="1280" src="https://player.twitch.tv/?channel=devilboy60&muted=true" frameborder="0" scrolling="no" allowfullscreen="true"></iframe>
+                <iframe height="720" width="1280" src="https://player.twitch.tv/?channel=60&muted=true" frameborder="0" scrolling="no" allowfullscreen="true"></iframe>
             </div>
         </div>
         <div class="col s12 m4 l4 chat">
